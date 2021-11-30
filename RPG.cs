066ced6202a86d70eba9c10b07f5044ba1020af9
Vadikray <66206477+Vadikray@users.noreply.github.com>
2021-11-30 19:37:21 +0500

    Init

---
+++ RPG.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class RPG : Weapon
+{
+    [SerializeField] private float _cooldown;
+    
+    private bool _recharge = false;
+
+    public override void Shoot(Transform shootPoint)
+    {
+        if (!_recharge)
+        {
+            _recharge = !_recharge;
+            Instantiate(Bullet, shootPoint.position, Quaternion.identity);
+            Invoke("ReloadWeapon", _cooldown);
+        }
+    }
+    
+    private void ReloadWeapon()
+    {
+        _recharge = !_recharge;
+    }
+}
---
+++ RPGBullet.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class RPGBullet : Bullet
+{
+    [SerializeField] private float _radiusExplosion;
+    [SerializeField] private LayerMask _interaction;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Explode();
+    }
+
+    private void Explode()
+    {
+        Destroy(gameObject);
+        var hitCollides = Physics2D.OverlapCircleAll(transform.position, _radiusExplosion, _interaction);
+
+        if (hitCollides == null)
+            return;
+
+        foreach (var hitCollide in hitCollides)
+        {
+            if (!hitCollide.TryGetComponent(out Enemy enemy))
+                continue;
+            if (enemy != null)
+                enemy.TakeDamage(_damage);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(transform.position, _radiusExplosion);
+    }
+}
